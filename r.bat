2fb44767b01da5b05af1a85656a65c59a6c5b7cd
Matthew Davies <mattie.davies@gmail.com>
2019-06-17 16:06:05 -0400

    Added some graphics.

--- r.bat
+++ r.bat
@@ -2,5 +2,5 @@
 call m.bat
 if not errorlevel 1 (
     bin\hdfmonkey put \sdcard\cspect-next-2gb.img demo.nex
-    bin\CSpect.exe -brk -s14 -w3 -zxnext -nextrom -map=demo.nex.map -mmc=\sdcard\cspect-next-2gb.img
+    bin\CSpect.exe -r -brk -s14 -w3 -zxnext -nextrom -map=demo.nex.map -mmc=\sdcard\cspect-next-2gb.img
 )
